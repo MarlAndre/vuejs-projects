e3046a2a8311fb2f751d772d64cb7398358f21cd
Marlene Andre <andre.marlene1990@gmail.com>
2022-09-08 15:59:23 -0400

    added battle log logic to display attack damage and heal restore

--- project-monster-slayer/app.js
+++ project-monster-slayer/app.js
@@ -10,6 +10,7 @@ const app = Vue.createApp({
       monsterHealth:100,
       currentRound: 0,
       winner: null,
+      logMessages: []
     }
   },
   computed: {
@@ -34,23 +35,28 @@ const app = Vue.createApp({
       this.playerHealth = 100,
       this.monsterHealth = 100,
       this.currentRound = 0,
-      this.winner = null
+      this.winner = null,
+      this.logMessages = []
     },
     attackMonster() {
       this.currentRound++
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
+      this.addLogMessage('player', 'attack', attackValue)
       this.attackPlayer()
 
     },
     attackPlayer() {
       const attackValue = getRandomValue(8, 15);
       this.playerHealth -= attackValue;
+      this.addLogMessage('monster', 'attack', attackValue)
+
     },
     specialAttackMonster() {
       this.currentRound++
       const attackValue = getRandomValue(10, 25);
       this.monsterHealth -= attackValue;
+      this.addLogMessage('player', 'special attack', attackValue)
       this.attackPlayer()
     },
     healPlayer() {
@@ -62,13 +68,20 @@ const app = Vue.createApp({
       } else {
         this.playerHealth += healValue
       }
+      this.addLogMessage('player', 'heal', attackValue)
       this.attackPlayer()
 
     },
     surrender() {
       this.winner = 'monster'
+    },
+    addLogMessage(who, what, value) {
+      this.logMessages.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value
+      })
     }
-
   },
   watch: {
     playerHealth(value) {
